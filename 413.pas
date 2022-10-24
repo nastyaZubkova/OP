bbf7ba728a099c9a0763233c5669f275cbf9d5cb
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2022-10-24 17:30:07 +0300

    lab3

---
+++ 413.pas
@@ -0,0 +1,23 @@
+PROGRAM PaulRevere(INPUT, OUTPUT);
+{Печать соответствующего сообщения, зависящего от величины
+ на входе:  '...by land' для 1 и '...by sea' для 2
+ иначе печать сообщения об ошибке}
+VAR
+  Lanterns: CHAR;
+BEGIN {PaulRevere}
+  {Read Lanterns}
+  READ(Lanterns);
+  {Issue Paul Revere's message}
+  IF Lanterns = 'T'
+  THEN
+    BEGIN
+      READ(Lanterns);
+      IF Lanterns = 'T'
+      THEN
+        WRITELN('The British are coming by sea.')
+      ELSE
+        WRITELN('The British are coming by land.')
+    END
+  ELSE
+    WRITELN('The North Church shows only ''', Lanterns, '''.')
+END. {PaulRevere}                               

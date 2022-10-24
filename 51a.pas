572b0f1cdb162dfb2e3f33e24dd4274ff5607ac5
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2022-10-24 17:34:50 +0300

    lab5

---
+++ 51a.pas
@@ -0,0 +1,24 @@
+PROGRAM IFSort3(INPUT, OUTPUT);
+{Сортирует 3-строку из INPUT в OUTPUT}
+VAR
+  Ch1, Ch2, Ch3: Char;
+BEGIN {IFSort3}
+  READ(Ch1, Ch2, Ch3);
+  WRITELN('Входные данные ', Ch1, Ch2, Ch3);
+  WRITE('Cортированные данные ');  
+  BEGIN {сортируем Ch1, Ch2, Ch3 в OUTPUT }
+    IF Ch1 < Ch2
+    THEN {Ch1 < Ch2:сортируем Ch1, Ch2, Ch3 в OUTPUT}
+      IF Ch2 < Ch3
+      THEN {Ch1 < Ch2 < Ch3:сортируем Ch1, Ch2, Ch3 в OUTPUT}
+        WRITELN(Ch1, Ch2, Ch3)
+      ELSE {Ch1 < Ch2, Ch3 <= Ch2:сортируем Ch1, Ch2, Ch3 в OUTPUT}
+        WRITELN('временно отсутствуют: условие 2 не выполняется')
+    ELSE {Ch2 <= Ch1:сортируем Ch1, Ch2, Ch3 т OUTPUT}
+      IF Ch1 < Ch3     
+        THEN {Ch2 <= Ch1 < Ch3:сортируем Ch1, Ch2, Ch3 в OUTPUT}
+          WRITELN(Ch2, Ch1, Ch3)
+       ELSE {Ch2 <= Ch1, Ch3 <= Ch1:сортируем Ch1, Ch2, Ch3 в OUTPUT}
+         WRITELN('временно отсутствуют: условие 3 не выполняется')
+  END
+END. {IFsort3}

a38aaca5ee3b5b9812ec300e4e7a2488e5f975a3
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2022-12-12 22:16:28 +0300

    Add files via upload

---
+++ 742.pas
@@ -0,0 +1,94 @@
+PROGRAM BubbleSort(INPUT, OUTPUT);
+{Сортируем первую строку INPUT в OUTPUT}
+VAR
+  Sorted, Ch, Ch1, Ch2: CHAR;
+  F1, F2: TEXT;
+BEGIN {BubbleSort}
+  { Копируем INPUT в F1 }
+  REWRITE(F1);
+  WHILE NOT EOF
+  DO
+    BEGIN
+      READ(INPUT, Ch);
+      WRITE(F1, Ch);
+      IF EOLN
+      THEN
+        WRITELN(F1)
+    END;
+  Sorted := 'N';
+  WHILE Sorted = 'N'
+  DO              
+    BEGIN
+      { Копируем F1 в F2,проверяя отсортированность
+       и переставляя первые соседние символы по порядку}
+      Sorted := 'Y';
+      RESET(F1);
+      REWRITE(F2);
+      WHILE NOT EOF(F1)
+      DO
+        BEGIN
+          IF NOT EOLN(F1)
+          THEN
+            BEGIN
+              READ(F1, Ch1);
+              WHILE NOT EOLN(F1)
+              DO { По крайней мере два символа остается для Ch1,Ch2 }
+                BEGIN
+                  READ(F1, Ch2);
+              { Выводим   min(Ch1,Ch2) в  F2, записывая
+               отсортированные символы }
+                  IF Ch1 <= Ch2
+                  THEN
+                    BEGIN
+                      WRITE(F2, Ch1);
+                      Ch1 := Ch2
+                    END
+                  ELSE
+                    BEGIN
+                      WRITE(F2, Ch2);
+                      Sorted := 'N'
+                    END
+                END;
+              WRITELN(F2, Ch1); { Выводим последний символ в F2 }
+              READLN(F1)
+            END
+        END;
+  { Копируем F2 в F1 }
+        RESET(F2);
+        REWRITE(F1);
+        WHILE NOT EOF(F2)
+        DO
+          BEGIN
+            IF NOT EOLN(F2)
+            THEN
+            BEGIN
+              READ(F2, Ch);
+              WRITE(F1, Ch);
+            END
+            ELSE
+              BEGIN
+                WRITELN(F1);
+                READLN(F2)
+              END 
+          END
+    END; 
+  { Копируем F1 в OUTPUT }
+  RESET(F1);
+  WHILE NOT EOF(F1)
+  DO
+    BEGIN
+      IF NOT EOLN(F1)
+      THEN
+        BEGIN
+          READ(F1, Ch);
+          IF Ch <> ' '
+          THEN
+            WRITE(OUTPUT, Ch)
+        END
+      ELSE
+        BEGIN
+          WRITELN(OUTPUT);
+          READLN(F1)
+        END 
+    END
+END. {BubbleSort}

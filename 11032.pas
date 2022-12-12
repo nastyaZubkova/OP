eff12c4a1e6cfe3b57abf25586b6ec8b7744ae32
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2022-12-12 20:48:21 +0300

    Add files via upload

---
+++ 11032.pas
@@ -0,0 +1,52 @@
+PROGRAM MinusLastChar(INPUT, OUTPUT);
+VAR
+  Ch1: CHAR;
+  F1, F2: TEXT;
+BEGIN
+  REWRITE(F1);
+  IF NOT EOLN
+  THEN
+    BEGIN
+      WHILE NOT EOLN
+      DO
+        BEGIN
+          READ(Ch1);
+          IF NOT EOLN
+          THEN                    
+            WRITE(F1, Ch1)
+        END;
+      WRITE(OUTPUT, Ch1)              
+    END;
+    RESET(F1);
+    WHILE NOT EOLN(F1)
+    DO
+      BEGIN
+        REWRITE(F2);
+        WHILE NOT EOLN(F1)
+        DO
+        BEGIN
+          READ(F1, Ch1);                                 
+          IF NOT EOLN(F1)
+          THEN                                  
+            WRITE(F2, Ch1)          
+        END;
+        WRITE(OUTPUT, Ch1);
+        RESET(F2);
+        REWRITE(F1);
+        IF NOT EOLN(F2)
+        THEN
+          BEGIN
+            WHILE NOT EOLN(F2)
+            DO
+            BEGIN
+              READ(F2, Ch1);                                 
+              IF NOT EOLN(F2)
+              THEN                                  
+                WRITE(F1, Ch1)           
+            END;
+            WRITE(OUTPUT, Ch1);
+            RESET(F1)
+          END
+      END;    
+  WRITELN      
+END.

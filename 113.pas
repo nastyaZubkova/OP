e0b07df20b5a97adc6e1a64d9f04d8634abe85c4
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2022-12-20 11:17:57 +0300

    Add files via upload

--- 113.pas
+++ 113.pas
@@ -7,51 +7,39 @@ BEGIN
   WHILE NOT EOLN
   DO
     BEGIN
-      READ(Ch1);
-      IF EOLN
-      THEN                                  
-        WRITE(Ch1);
-      IF NOT EOLN
-      THEN                                  
-        WRITE(F1, Ch1);           
+      READ(Ch1);                 
+      WRITE(F1, Ch1)
     END;
-    RESET(F1);
-    REWRITE(F2);
-    WHILE NOT EOLN(F1)
-    DO
+  RESET(F1);
+  WHILE NOT EOLN(F1)
+  DO
     BEGIN
-      READ(Ch1);
-      IF EOLN
-      THEN                                  
-        WRITE(OUTPUT, Ch1);
-      IF NOT EOLN
-      THEN                                  
-        WRITE(F2, Ch1);           
-    END;
-    RESET(F2);
-    REWRITE(F1);
-    WHILE NOT EOLN(F2)
-    DO
-    BEGIN
-      READ(Ch1);
-      IF EOLN
-      THEN                                  
-        WRITE(OUTPUT, Ch1);
-      IF NOT EOLN
-      THEN                                  
-        WRITE(F1, Ch1);           
+      REWRITE(F2);
+      WHILE NOT EOLN(F1)
+      DO
+        BEGIN
+          READ(F1, Ch1);                                 
+          IF NOT EOLN(F1)
+          THEN                                  
+            WRITE(F2, Ch1)          
+        END;
+      WRITE(OUTPUT, Ch1);
+      RESET(F2);
+      REWRITE(F1);
+      IF NOT EOLN(F2)
+      THEN
+        BEGIN
+          WHILE NOT EOLN(F2)
+          DO
+            BEGIN
+              READ(F2, Ch1);                                 
+              IF NOT EOLN(F2)
+              THEN                                  
+                WRITE(F1, Ch1)           
+            END;
+          WRITE(OUTPUT, Ch1);
+          RESET(F1)
+        END
     END;    
-    RESET(F1);
-    WHILE NOT EOLN(F1)
-    DO
-    BEGIN
-      READ(Ch1);
-      IF EOLN
-      THEN                                  
-        WRITE(OUTPUT, Ch1);
-      IF NOT EOLN
-      THEN                                  
-        WRITE(OUTPUT, Ch1);           
-    END;    
-  WRITELN    
-END.      
+  WRITELN      
+END.

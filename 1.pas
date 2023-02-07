437c94c3d346c3dd9bddada226774d59f6f7aefc
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2023-02-07 11:34:14 +0300

    13.1

---
+++ 1.pas
@@ -0,0 +1,35 @@
+PROGRAM RunBubbleSort1(INPUT, OUTPUT);
+ { Сортируем первую строку INPUT в OUTPUT }
+VAR
+  Sorted: CHAR;
+  F1, F2: TEXT;
+PROCEDURE CopyFile(VAR InFile, OutFile: TEXT);
+VAR
+  Ch: CHAR;
+BEGIN
+  WHILE NOT EOLN(InFile)
+  DO
+    BEGIN
+      READ(InFile, Ch);
+      WRITE(OutFile, Ch)
+    END;
+  WRITELN(OutFile)
+END;  
+BEGIN { BubbleSort }
+  { Копируем INPUT в F1 }
+  REWRITE(F1);
+  CopyFile(INPUT, F1);
+  Sorted := 'N';
+  WHILE Sorted = 'N'
+  DO
+    BEGIN
+      Sorted := 'F'
+      { Копируем F1 в F2,проверяя отсортированность
+       и переставляя первые соседние символы по порядку}
+      { Копируем F2 в F1 }
+    END;
+ { Копируем F1 в OUTPUT }
+  RESET(F1);
+  CopyFile(F1, OUTPUT)
+END.{ BubbleSort }
+

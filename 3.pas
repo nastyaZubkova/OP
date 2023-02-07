468a61952d45732b2b9975ea910afe692e13a0de
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2023-02-07 11:34:58 +0300

    13.3

---
+++ 3.pas
@@ -0,0 +1,70 @@
+PROGRAM RunBubbleSort3(INPUT, OUTPUT);
+PROCEDURE CopyFile(VAR InFile, OutFile: TEXT);
+VAR
+  Ch: CHAR;
+BEGIN
+  WHILE NOT EOLN(InFile)
+  DO
+    BEGIN
+      READ(InFile, Ch);
+      WRITE(OutFile, Ch)
+    END;
+  WRITELN(OutFile)
+END;
+PROCEDURE CopyAndSwap(VAR F1, F2: TEXT; VAR Sorted: CHAR);
+VAR 
+  Ch1, Ch2: CHAR;
+BEGIN
+  Sorted := 'Y';
+  IF NOT EOLN(F1)
+  THEN
+    BEGIN
+      READ(F1, Ch1);
+      WHILE NOT EOLN(F1)
+      DO { По крайней мере два символа остается для Ch1,Ch2 }
+        BEGIN
+          READ(F1, Ch2);
+        { Выводим   min(Ch1,Ch2) в  F2, записывая
+         отсортированные символы }
+          IF Ch1 <= Ch2
+          THEN
+            BEGIN
+              WRITE(F2, Ch1);
+              Ch1 := Ch2
+            END
+          ELSE
+            BEGIN
+              WRITE(F2, Ch2);
+              Sorted := 'N'
+            END
+        END;
+      WRITELN(F2, Ch1) { Выводим последний символ в F2 }
+    END
+END;
+PROCEDURE BubbleSort(VAR InFile, OutFile: TEXT);
+VAR
+  Sorted: CHAR;
+  F1, F2: TEXT;
+BEGIN
+  REWRITE(F1);
+  CopyFile(InFile, F1);
+  Sorted := 'N';
+  WHILE Sorted = 'N'
+  DO
+    BEGIN
+      RESET(F1);
+      REWRITE(F2);
+      CopyAndSwap(F1, F2, Sorted);
+      { Копируем F2 в F1 }
+      RESET(F2);
+      REWRITE(F1);      
+      CopyFile(F2, F1)  
+    END;
+  RESET(F1);
+  CopyFile(F1, OutFile)
+END;  
+BEGIN { BubbleSort }
+  BubbleSort(INPUT, OUTPUT)
+END.{ BubbleSort }
+
+

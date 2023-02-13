1fa63de3bbcb9888ea93a32cd27b024cbb183145
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2023-02-14 00:09:07 +0300

    lab13

---
+++ 6.pas
@@ -0,0 +1,63 @@
+PROGRAM Split(INPUT,OUTPUT);
+  {Копирует INPUT в OUTPUT,сначала нечетные,а затем четные
+   элементы}
+VAR
+  Ch,Next: CHAR;
+  Odds,Evens: TEXT; 
+{PROCEDURE CopyOut(VAR F1: TEXT; VAR Ch: CHAR);}
+PROCEDURE CopyOut(VAR F1: TEXT; VAR Ch: CHAR);
+BEGIN
+  {Копируем F1 в OUTPUT}
+  WHILE NOT EOF(F1)
+  DO
+    BEGIN
+      WHILE NOT EOLN(F1)
+      DO
+        BEGIN
+          READ(F1, Ch);
+          WRITE(OUTPUT, Ch)
+        END;
+      READLN(F1) 
+    END
+END;
+BEGIN
+  {Разделяет INPUT в Odds и Evens}
+  BEGIN
+    REWRITE(Odds);
+    REWRITE(Evens);
+    Next := 'O';
+    WHILE NOT EOF
+    DO
+      BEGIN
+        WHILE NOT EOLN
+        DO
+          {Прочитать Ch, записать в файл, выбранный через
+           Next,переключить Next}
+          BEGIN 
+            READ(INPUT, Ch);
+            IF Next = 'O'
+            THEN
+              BEGIN 
+                WRITE(Odds, Ch);
+                Next := 'E'
+              END
+            ELSE
+              BEGIN
+                WRITE(Evens, Ch); 
+                Next := 'O'
+              END
+          END; 
+        READLN;
+        WRITELN(Odds);
+        WRITELN(Evens)
+      END;
+    WRITELN(Odds);
+    WRITELN(Evens)
+  END;
+  RESET(Odds);
+  RESET(Evens);
+  CopyOut(Odds,Ch);
+  CopyOut(Evens,Ch);
+  WRITELN
+END.
+

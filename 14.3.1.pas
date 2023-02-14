6e78f94dda2d5b9cf24961baa28955d9492f08ea
nastyaZubkova <116561172+nastyaZubkova@users.noreply.github.com>
2023-02-14 14:07:10 +0300

    lab14
    первый этап сборки, не работает

---
+++ 14.3.1.pas
@@ -0,0 +1,73 @@
+PROGRAM Test(INPUT, OUTPUT);  
+  
+PROCEDURE CopyFile(VAR InFile, OutFile: TEXT);
+VAR
+  Ch: CHAR;
+BEGIN
+  WHILE NOT EOLN(InFile)
+  DO
+    BEGIN
+      READ(InFile, Ch);
+      WRITE(OutFile, Ch)
+    END
+END;  
+
+PROCEDURE Split(VAR F1, F2, F3: TEXT);
+{Разбивает F1 на F2, F3}
+VAR 
+  Ch, Switch: CHAR;
+BEGIN {Split}
+  RESET(F1);
+  REWRITE(F2);
+  REWRITE(F3);
+  {Копировать F1 попеременно в F2 и F3}
+  Switch := '2';
+  WHILE NOT (EOLN(F1))
+  DO
+    BEGIN
+      READ(F1, Ch);
+      IF (Switch = '2')
+      THEN
+        BEGIN
+          WRITE(F2, Ch);
+          Switch := '3';
+        END
+      ELSE
+        BEGIN
+          WRITE(F3, Ch);
+          Switch := '2';
+        END
+    END;
+  WRITELN(F2);
+  WRITELN(F3)
+END; {Split}
+
+PROCEDURE RecursiveSort(VAR F1: TEXT);
+VAR 
+  F2, F3: TEXT;
+  Ch: CHAR;
+BEGIN {RecursiveSort}
+  RESET(F1);
+  IF NOT (EOLN(F1))
+  THEN
+    BEGIN
+      IF NOT (EOLN(F1))
+      THEN {Файл имеет как минимум 2 символа}
+        BEGIN
+          Split(F1, F2, F3);
+          {RecursiveSort(F2);
+          RecursiveSort(F3);
+          Merge(F1, F2, F3); }
+          RESET(F2);
+          RESET(F3);
+          REWRITE(F1);
+          CopyFile(F2, F1);
+          CopyFile(F3, F1);
+          RESET(F1);
+          CopyFile(F1, OUTPUT);
+        END
+    END
+END;   {RecursiveSort}
+BEGIN {Test}
+  RecursiveSort(INPUT);
+END.{Test}
